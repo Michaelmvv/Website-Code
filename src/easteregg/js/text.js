cecaeb9a398a323df6d5a43e08b60672fa3a271c
ThorinDev <c.nethercott@icloud.com>
2015-09-30 17:48:16 +0100

    the dream is dead :((((((

--- src/easteregg/js/text.js
+++
@@ -1,6 +0,0 @@
-// Just some text script
-
-// Text Boxes
-document.getElementById("header").innerHTML = "Welcome to the Easter Egg Hunt!";
-document.getElementById("header_info").innerHTML = "Enter your answer to the previous clue below!";
-document.getElementById("textbox_info").innerHTML = "All of this text is being loaded with an external script because I <3 JS and I am trying to learn it better";
